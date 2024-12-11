ac7f6d500014e7f995287b3e810df2bcbb276d20
nia <1205367274@qq.com>
2024-12-11 21:45:22 +0800

    feat: scanning: parentheses

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,21 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            // writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
-
             // Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                for c in file_contents.chars() {
+                    if c == '(' {
+                        println!("LEFT_PAREN ( null");
+                    } else if c == ')' {
+                        println!("RIGHT_PAREN ) null");
+                    }
+                }
             } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+                // Placeholder, remove this line when implementing the scanner
             }
         }
         _ => {
@@ -34,4 +36,5 @@ fn main() {
             return;
         }
     }
+    println!("EOF  null");
 }
